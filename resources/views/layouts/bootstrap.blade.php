d20d497e82bdb137b2db947e5a5f32e2d59b7fb8
dwi2febri <mochdwifebri24@gmail.com>
2026-03-18 09:03:26 +0700

    update tampilan

--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{ config('app.name','Pipeline Prospek') }}</title>
+  <title>{{ config('app.name','E-Prospek') }}</title>
 
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @livewireStyles
